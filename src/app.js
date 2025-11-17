9ffee7de0ab48233d022b6d3032ea6e0fdefab6c
Lluis Alsina Navarro <lalsina883@gmail.com>
2025-11-17 08:15:52 +0100

    Modificaciones en app.js para intentar solucionar CORS

--- src/app.js
+++ src/app.js
@@ -10,12 +10,30 @@ import apiRoutes from './routes/api.routes.js';
 
 const app = express();
 
-app.use(express.json());
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: (origin, callback) => {
+        callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.use(express.json());
+
+app.options('/', handleOptions);
+app.options('/api', handleOptions);
+app.options('/api/trips', handleOptions);
+app.options('/api/users', handleOptions);
+app.options('/api/images', handleOptions);
+
+function handleOptions(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Max-Age', '86400');
+    return res.status(200).end();
+}
 
 app.use('/api', apiRoutes);
 
@@ -28,8 +46,8 @@ app.use((req, res, next) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
 });
 
 export default app;
